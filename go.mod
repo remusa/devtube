e1afbdae4433ccbbbbfa38e3ad1ad966b261d2df
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-12 22:03:01 -0600

    chore: create chi server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/remusa/devtube
 
 go 1.21.1
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -18,5 +17,16 @@ func main() {
 		log.Fatal("PORT not found in environment")
 	}
 
-	fmt.Println("PORT:", portString)
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server started on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
