b1ff81e05b9417534c051388444704969893b42c
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-12 21:52:31 -0600

    chore: read env vars from .env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/remusa/devtube
 
 go 1.21.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in environment")
+	}
+
+	fmt.Println("PORT:", portString)
 }
