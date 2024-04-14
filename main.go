707e527bac9dd2258610491102d1014eb11561b6
R Sánchez <you@example.com9169831+remusa@users.noreply.github.com>
2024-04-14 14:03:56 -0600

    chore: use server mux

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"reflect"
 	"time"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
@@ -43,43 +43,41 @@ func main() {
 		DB: dbQueries,
 	}
 
-	go startScraping(dbQueries, 10, time.Minute)
+	mux := http.NewServeMux()
 
-	router := chi.NewRouter()
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("root")
+	})
 
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"*"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
-	}))
+	mux.HandleFunc("POST v1/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("GET v1/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
 
-	v1Router := chi.NewRouter()
+	mux.HandleFunc("POST v1/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedsCreate))
+	mux.HandleFunc("GET v1/feeds", apiCfg.handlerFeedsGet)
 
-	v1Router.Post("/users", apiCfg.handlerUsersCreate)
-	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+	mux.HandleFunc("GET v1/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
 
-	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedsCreate))
-	v1Router.Get("/feeds", apiCfg.handlerFeedsGet)
-	v1Router.Delete("/feeds/{feedID}", apiCfg.middlewareAuth(apiCfg.handlerFeedsDelete))
+	mux.HandleFunc("GET v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet))
+	mux.HandleFunc("POST v1/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsCreate))
+	mux.HandleFunc("DELETE v1/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsDelete))
 
-	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
+	mux.HandleFunc("GET v1/ready", handlerReadiness)
+	mux.HandleFunc("GET v1/err", handlerErr)
 
-	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsCreate))
-	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsGet))
-	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerFeedFollowsDelete))
+	corsMux := corsMiddleware(mux)
 
-	v1Router.Get("/ready", handlerReadiness)
-	v1Router.Get("/err", handlerErr)
-
-	router.Mount("/v1", v1Router)
-	srv := &http.Server{
+	server := &http.Server{
 		Addr:    ":" + port,
-		Handler: router,
+		Handler: corsMux,
 	}
 
+	const collectionConcurrency = 10
+	const collectionInterval = time.Minute
+	go startScraping(dbQueries, collectionConcurrency, collectionInterval)
+
+	v := reflect.ValueOf(http.DefaultServeMux).Elem()
+	fmt.Printf("routes: %v\n", v.FieldByName("mux121").FieldByName("m"))
 	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	err = server.ListenAndServe()
+	log.Fatal(err)
 }
